87297c063587d0bad954f63a9b645d4d5e197428
Timur Musin <musin_timur@samaradom.ru>
2026-04-02 14:09:09 +0400

    Решение проблемы N+1

--- src/main/kotlin/ru/musintimur/eventmanager/repository/EventRepository.kt
+++ src/main/kotlin/ru/musintimur/eventmanager/repository/EventRepository.kt
@@ -2,6 +2,7 @@ package ru.musintimur.eventmanager.repository
 
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
@@ -10,18 +11,22 @@ import ru.musintimur.eventmanager.domain.EventStatus
 
 interface EventRepository : JpaRepository<Event, Long> {
     /** Главная страница: только одобренные мероприятия, сортировка по дате */
+    @EntityGraph(attributePaths = ["organizer"])
     fun findAllByStatus(
         status: EventStatus,
         pageable: Pageable,
     ): Page<Event>
 
     /** Список мероприятий на рассмотрении у менеджера */
+    @EntityGraph(attributePaths = ["organizer"])
     fun findAllByStatus(status: EventStatus): List<Event>
 
     /** Мероприятия конкретного организатора */
+    @EntityGraph(attributePaths = ["organizer"])
     fun findAllByOrganizerId(organizerId: Long): List<Event>
 
     /** Поиск одобренного мероприятия по id (для публичного просмотра) */
+    @EntityGraph(attributePaths = ["organizer"])
     fun findByIdAndStatus(
         id: Long,
         status: EventStatus,
