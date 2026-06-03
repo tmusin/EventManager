0c1953c5dd08860afad5f2a16d96940c74c6df9f
Timur Musin <musin_timur@samaradom.ru>
2026-06-03 13:41:03 +0400

    Обновлена версия Testcontainers

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,9 +54,9 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
     // Testcontainers
-    testImplementation("org.testcontainers:testcontainers:1.20.4")
-    testImplementation("org.testcontainers:postgresql:1.20.4")
-    testImplementation("org.testcontainers:junit-jupiter:1.20.4")
+    testImplementation("org.testcontainers:testcontainers:1.21.4")
+    testImplementation("org.testcontainers:postgresql:1.21.4")
+    testImplementation("org.testcontainers:junit-jupiter:1.21.4")
 }
 
 kotlin {
